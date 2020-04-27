101cd42461b039f0068a4b3582e8784710409256
Vaibhav229 <61175944+Vaibhav229@users.noreply.github.com>
2020-04-27 12:39:02 +0530

    Add files via upload

---
+++ Users.java
@@ -0,0 +1,75 @@
+package com.capgemini.entities;
+
+public class Users
+		/**
+		 * Class Users
+		 */
+{
+	int userId;
+	String userName;
+	String userPassword;
+	long userPhone;
+	String userEmail;
+	String userType;
+	
+	public Users(int userId, String userName, String userPassword, long userPhone, String userEmail, String userType)
+	/**
+	 * Parameterized Constructors of Users
+	 */
+	{
+		this.userId = userId;
+		this.userName = userName;
+		this.userPassword = userPassword;
+		this.userPhone = userPhone;
+		this.userEmail = userEmail;
+		this.userType = userType;
+	}
+	public Users()
+	/**
+	 * Unparameterized Constructor of User
+	 */
+	{
+	}
+
+
+
+	public String getUserType() {
+		return userType;
+	}
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	public long getUserPhone() {
+		return userPhone;
+	}
+	public void setUserPhone(long userPhone) {
+		this.userPhone = userPhone;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	
+	
+	
+}
