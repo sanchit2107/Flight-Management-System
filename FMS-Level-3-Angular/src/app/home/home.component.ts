4032bda520191323b010c33bf6043f256831700d
sanchit2107 <sanchit2107@gmail.com>
2020-05-08 04:17:48 +0530

    Creating and designing home component

---
+++ FMS-Level-3-Angular/src/app/home/home.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Airport } from '../model/app.airport';
+import { AirportService } from '../services/app.airportservice';
+
+@Component({
+  selector: 'home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  airports:Airport[]=[];
+
+    src:string;
+    dest:string;
+    journeyDate:string;
+
+    minDate:any;
+    maxDate:any;
+
+    errorFlag:boolean=false;
+
+    constructor(private router:Router, private airportService:AirportService){
+        this.minDate=new Date();
+        this.maxDate=new Date();
+        this.maxDate.setMonth(this.maxDate.getMonth()+2);
+    }
+
+    ngOnInit(){
+        this.airportService.getAllAirports().subscribe((data:Airport[])=>this.airports=data);
+    }
+
+    findFlights(){
+        this.router.navigate(['/booking/availableflights', this.src, this.dest, this.journeyDate]);
+    }
+
+    airportCheck(){
+        if(this.src===this.dest){
+            this.errorFlag=true;
+        }else{
+            this.errorFlag=false;
+        }
+    }
+
+    sendMessage(){
+        alert("Thank You for reaching out to us.");
+        this.router.navigate(["/home"]);
+    }
+
+
+}
---
+++ FMS-Level-3-Angular/src/app/model/app.airport.ts
@@ -0,0 +1,6 @@
+export class Airport{
+
+  airportName:string;
+  airportCode:string;
+  airportLocation:string;
+}
