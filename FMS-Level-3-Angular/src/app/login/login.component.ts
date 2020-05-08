122e2ff929a3d6bccb7ec7bec366ec30490e453f
sanchit2107 <sanchit2107@gmail.com>
2020-05-08 21:44:26 +0530

    Adding LogIn component

---
+++ FMS-Level-3-Angular/src/app/login/login.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../model/app.user';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../services/authentication.service';
+import { ThrowStmt } from '@angular/compiler';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  username = 'admin';
+  password = '';
+  user: User={"userId": 0, "userName":"", "userPassword":"", "userPhone": 0, "email":"", "active": null, "roles":""};
+  invalidLogin = false;
+
+  constructor(private router: Router,
+    private loginservice: AuthenticationService) { }
+
+  ngOnInit(): void {
+  }
+
+  // Check user for authenticatoin
+  checkLogin() {
+    if(this.loginservice.authenticate(this.username, this.password)) {
+      this.loginservice.getRole(this.username).subscribe((data: User)=> {
+        this.user = data;
+        this.redirect();
+      });
+    }
+    else {
+      console.log("Invalid Login Credentials..");
+      this.invalidLogin = true;
+    }
+  }
+
+  // Redirect based on the user role
+  redirect() {
+    if(this.user.roles === 'ROLE_CUSTOMER') {
+      sessionStorage.setItem('role', 'customer');
+      sessionStorage.setItem('userId', String(this.user.userId));
+      this.invalidLogin = false;
+      this.router.navigate(["/userpanel"]).then(()=> {
+        window.location.reload();
+      });
+    }
+    else if(this.user.roles === 'ROLE_ADMIN') {
+      sessionStorage.setItem('role', 'admin');
+      sessionStorage.setItem('userId', String(this.user.userId));
+      this.invalidLogin = false;
+      this.router.navigate(["adminpanel"]).then(()=> {
+        window.location.reload();
+      });
+    }
+  }
+
+}
